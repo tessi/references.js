7b0efaeaab0689b394a4eec26e252ac220416c5d
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2012-03-06 16:54:48 +0100

    use numbers as a reference

--- references.js
+++ references.js
@@ -1,7 +1,8 @@
 /**
  * references.js
  *
- * impress.js is a ...
+ * references.js is a small JavaScript library that lets you define your sources
+ * in a scientific paper like style.
  *
  * MIT Licensed.
  *
@@ -23,20 +24,26 @@
 
   var reference_list = [].slice.call(  document.querySelectorAll(".reference")  ),
       references_list = [].slice.call( document.querySelectorAll(".references") ),
-      reference_map = {};
+      reference_map = {},
+      reference_tag;
 
   reference_list.forEach(function ( el, idx ) {
     if ( el.dataset.name ) {
-      reference_map[el.dataset.name] = el.dataset.name;
+      reference_map[el.dataset.name] = idx + 1;
+
+      reference_tag = document.createElement("span");
+      reference_tag.setAttribute("class", "reference-count-source");
+      reference_tag.innerHTML = "[" + reference_map[el.dataset.name] + "]";
+      insertBefore(reference_tag, el);
     };
   });
 
   references_list.forEach(function ( el, idx ) {
     if ( el.dataset.name ) {
-      var ref = document.createElement("span");
-      ref.setAttribute("class", "reference-count");
-      ref.innerHTML = "[" + reference_map[el.dataset.name] + "]";
-      insertAfter(el, ref);
+      reference_tag = document.createElement("span");
+      reference_tag.setAttribute("class", "reference-count");
+      reference_tag.innerHTML = "[" + reference_map[el.dataset.name] + "]";
+      insertAfter(el, reference_tag);
     };
   });
 
